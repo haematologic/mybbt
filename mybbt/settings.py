596858d6f22a1b6f24cf48a6ed214589d7bc82b6
haematologic <haematologic@gmail.com>
2013-06-28 00:26:16 +0100

    fix S3boto class name typo in settings

--- mybbt/settings.py
+++ mybbt/settings.py
@@ -170,6 +170,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 if not DEBUG:
     # Production storage using s3.
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    STATICFILES_STORAGE = 'storages.backends.s3boto.s3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL + '/static/'
