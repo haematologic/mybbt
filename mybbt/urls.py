63b09350b76d58adf9fc1b3e01cae102fa225cb1
haematologic <haematologic@gmail.com>
2013-06-28 01:20:31 +0100

    fix static url ref

--- mybbt/urls.py
+++ mybbt/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns(”,
-(r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
+urlpatterns += patterns('',
+(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
