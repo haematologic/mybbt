d128002adced8a999049eeb6a4644d05d56e15da
haematologic <haematologic@gmail.com>
2013-06-28 01:30:58 +0100

    fix static url ref

--- mybbt/urls.py
+++ mybbt/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
