7ede4b21acb570e455589b336ca1b499caf634e5
haematologic <haematologic@gmail.com>
2013-07-08 19:23:27 +0100

    updated settings

--- mybbt/settings.py
+++ mybbt/settings.py
@@ -65,14 +65,14 @@ MEDIA_URL =
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = 'var/www/com.mybbt.static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -172,4 +172,5 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL + '/static/'
+    STATIC_URL = S3_URL + 'static/'
+    ADMIN_MEDIA_PREFIX = S3_URL + 'admin/'
