8bd5c957567407ac7523603564583c089963e147
haematologic <haematologic@gmail.com>
2013-07-09 12:06:50 +0100

    django 1.5 requires allowed_hosts for production env

--- mybbt/settings.py
+++ mybbt/settings.py
@@ -3,8 +3,6 @@
 DEBUG = False
 # TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*'] # Required for Django 1.5 production env
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -29,7 +27,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*'] #CHANGEME!
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
